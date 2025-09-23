d725821f0118dedd768b88224bccf7ed0a1e4306
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-22 22:53:52 -0500

    Algoritmo de busqueda en laberintos BFS y DFS

--- Algoritmos_Basicos_IA/laberinto_BFS_DFS.py
+++ Algoritmos_Basicos_IA/laberinto_BFS_DFS.py
@@ -12,3 +12,16 @@ laberinto = [
     [1, 1, 0, 0, 0],
     [0, 0, 0, 1, 0],
 ]
+inicio = (0, 0)  # fila, columna
+meta = (4, 4)
+
+# Movimientos posibles: arriba, abajo, izquierda, derecha
+movimientos = [(-1,0), (1,0), (0,-1), (0,1)]
+
+def vecinos(nodo):
+    fila, col = nodo
+    for df, dc in movimientos:
+        nf, nc = fila + df, col + dc
+        if 0 <= nf < len(laberinto) and 0 <= nc < len(laberinto[0]):
+            if laberinto[nf][nc] == 0:
+                yield (nf, nc)
