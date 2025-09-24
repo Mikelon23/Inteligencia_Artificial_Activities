74f14fbdab4f7dadd8ce324f85eb67315f08d133
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-24 00:05:43 -0500

    ejercicio de un algoritmo de ml tipo regresion lineal

--- ML_Supervisado/Regresion_Linela.py
+++ ML_Supervisado/Regresion_Linela.py
@@ -7,3 +7,12 @@ from sklearn.linear_model import LinearRegression
 # Datos ficticios: metros cuadrados vs precio en miles de dólares
 X = np.array([[50], [60], [80], [100], [120]])
 y = np.array([150, 180, 220, 260, 300])
+# Entrenar modelo
+modelo = LinearRegression()
+modelo.fit(X, y)
+
+# Predecir
+nueva_casa = np.array([[90]])
+prediccion = modelo.predict(nueva_casa)
+
+print(f"Precio estimado para una casa de 90 m²: {prediccion[0]:.2f} mil dólares")
