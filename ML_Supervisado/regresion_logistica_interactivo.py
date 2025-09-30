bf840927bad4813ae4c12c7a51692696e6b554a6
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-30 17:54:13 -0500

    Ejercicio de regresion logistiva interactivo

--- ML_Supervisado/regresion_logistica_interactivo.py
+++ ML_Supervisado/regresion_logistica_interactivo.py
@@ -1,2 +1,10 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+
+# Datos ficticios
+X = np.array([[1], [2], [3], [4], [5], [6], [7]])
+y = np.array([0, 0, 0, 1, 1, 1, 1])
+
+modelo = LogisticRegression()
+modelo.fit(X, y)
+
