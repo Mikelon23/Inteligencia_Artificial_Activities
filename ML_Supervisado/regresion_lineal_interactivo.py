3b52f94105778a6c277768195bea58f39b5f0184
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-29 23:14:22 -0500

    Ejercicio de regresion lineal interactivo

--- ML_Supervisado/regresion_lineal_interactivo.py
+++ ML_Supervisado/regresion_lineal_interactivo.py
@@ -1,3 +1,11 @@
 #Predice precio de casas según los m² que ingreses por teclado.
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+# Datos ficticios
+X = np.array([[50], [60], [80], [100], [120]])
+y = np.array([150, 180, 220, 260, 300])
+
+modelo = LinearRegression()
+modelo.fit(X, y)
+
