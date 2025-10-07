a855cf461ae8be9a2f5463145c01139b2fdd61af
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-10-06 22:40:59 -0500

    Ejercicio clasico de ML knn

---
+++ ML_Clasicos/knn.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+# Datos: [peso (g), textura (0 = rugosa, 1 = suave)]
+X = [
+    [150, 0],  # Naranja
+    [170, 0],  # Naranja
+    [140, 1],  # Manzana
+    [130, 1],  # Manzana
+]
