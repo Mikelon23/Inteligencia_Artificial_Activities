060f238fc853c1fa1083e863671c7673b24cd600
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-10-07 22:58:51 -0500

    Ejercicio clasico ML SVM

--- ML_Clasicos/SupportVectorMachine.py
+++ ML_Clasicos/SupportVectorMachine.py
@@ -1,3 +1,10 @@
 from sklearn import svm
 import matplotlib.pyplot as plt
 
+# Datos: coordenadas X, Y
+X = [[1, 2], [2, 3], [3, 3], [8, 8], [9, 9], [10, 10]]
+y = [0, 0, 0, 1, 1, 1]  # Grupo 0 y grupo 1
+
+modelo = svm.SVC(kernel='linear')
+modelo.fit(X, y)
+
