32317cd4401ee2a16a5ddf06d2c7420f515b7569
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-10-07 22:49:56 -0500

    Ejercicio clasico ML Arbol de decision

---
+++ ML_Clasicos/arbol_decision.py
@@ -0,0 +1,11 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+# Datos: [horas estudio, horas sueño]
+X = [
+    [2, 6],   # Reprueba
+    [4, 6],   # Aprueba
+    [6, 7],   # Aprueba
+    [1, 5],   # Reprueba
+]
