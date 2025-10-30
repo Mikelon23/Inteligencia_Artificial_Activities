771a3cd8a8b31cf830ea159e66280a4bcb7930cb
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-10-29 23:24:19 -0500

    Ejercicio de Deep Learning para reconocer digitos escritos a mano

--- DeepLearning/CNN_reconocer_dígitos_mano.py
+++ DeepLearning/CNN_reconocer_dígitos_mano.py
@@ -6,3 +6,7 @@ import matplotlib.pyplot as plt
 # Cargar dataset MNIST
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
+# Normalizar (0–255 → 0–1)
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
